f56d8292e5dde7535a27f7387ec622a1200415d8
SusuMantan <suzumantan@gmail.com>
2022-03-01 09:21:17 +0000

    finished

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,22 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Outfit", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        softBlue: "hsl(215, 51%, 70%)",
+        cyan: "hsl(178, 100%, 50%)",
+        veryDarkBlue: {
+          500: "hsl(215, 32%, 27%)",
+          600: "hsl(216, 50%, 16%)",
+          800: "hsl(217, 54%, 11%)",
+        },
+      },
+    },
   },
   plugins: [],
 };
